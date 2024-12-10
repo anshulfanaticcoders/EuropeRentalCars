e71964acaff3ab13146b1b666e870c205b234b9f
pankajwb <pankajwb@gmail.com>
2024-12-10 22:39:27 +0530

    Added Carreviews datatable

---
+++ app/Http/Controllers/CarReviewsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CarReviews;
+use App\Http\Requests\StoreCarReviewsRequest;
+use App\Http\Requests\UpdateCarReviewsRequest;
+
+class CarReviewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCarReviewsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CarReviews $carReviews)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CarReviews $carReviews)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCarReviewsRequest $request, CarReviews $carReviews)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CarReviews $carReviews)
+    {
+        //
+    }
+}
